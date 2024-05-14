6867b3ca50ab421824c595642d434bedaa938dfd
nedretcelik <nedretcelik97@gmail.com>
2024-05-13 20:37:00 -0400

    add cart array and implement map function

---
+++ forEach-and-map/cart.js
@@ -0,0 +1,18 @@
+let cart = [
+  { item: "Bread", price: 3.29, quantity: 2 },
+  { item: "Milk", price: 4.09, quantity: 1 },
+  { item: "T-Bone Steak", price: 12.99, quantity: 2 },
+  { item: "Baking Potato", price: 1.89, quantity: 6 },
+  { item: "Iceberg Lettuce", price: 2.06, quantity: 1 },
+  { item: "Ice Cream - Vanilla", price: 6.81, quantity: 1 },
+  { item: "Apples", price: 0.66, quantity: 6 },
+];
+
+
+// a. Write code that searches the courses array using the map () function to return only the item name and then use forEach () to display the list of items.
+
+let itemName = cart.map((items) => items.item).forEach(item => {
+    console.log(item)
+    
+});
+
